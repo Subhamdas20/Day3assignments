693f9e6657084fc2254e4a3d0476d1fcad144133
Subhamdas20 <subham21.sd@gmail.com>
2021-09-08 00:57:00 +0530

    Solution for uc1

---
+++ src/solutions/CalculateLength.java
@@ -0,0 +1,23 @@
+package solutions;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class CalculateLength {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of co-ordinate x1");
+        int x1=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate x2");
+        int x2=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate y1");
+        int y1=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate y1");
+        int y2=sc.nextInt();
+        double a= Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2)));
+
+        System.out.println("Length of line is -"+" " +a);
+
+
+    }
+}
+
--- src/solutions/DisplayWelcome.java
+++
@@ -1,7 +0,0 @@
-package solutions;
-
-public class DisplayWelcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to line compairison computation program ");
-    }
-}
