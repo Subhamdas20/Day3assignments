0c15ff29fab7da6a7d0a2f0bccb7f7f5dda766e5
Subhamdas20 <subham21.sd@gmail.com>
2021-09-08 02:23:31 +0530

    Display welcome message

---
+++ src/solutions/DisplayWelcome.java
@@ -0,0 +1,7 @@
+package solutions;
+
+public class DisplayWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+    }
+}
